c1beb4a63a3f3024b0046af8b3a93dcc8b0f8b00
ROGUEANOVI <ovidioromero66@gmail.com>
2023-11-04 22:36:12 -0500

    fix: Fix conflicts

---
+++ LibraryManagementApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManagementApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
